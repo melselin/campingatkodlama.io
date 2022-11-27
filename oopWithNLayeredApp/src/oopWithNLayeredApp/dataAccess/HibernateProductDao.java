6f4ec5faeadc9319c386aeb3d6a783ded14926bf
Melisa <melisaselinmusellim@gmail.com>
2022-11-27 22:09:52 +0300

    commit

--- oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
@@ -3,11 +3,11 @@ package oopWithNLayeredApp.dataAccess;
 import oopWithNLayeredApp.entities.Product;
 
 public class HibernateProductDao implements ProductDao {
-	
+
 	public void add(Product product) {
-		//<db access codes comes here> SQL
+		// <db access codes comes here> SQL
 		System.out.println("Added to the db via hibernate");
-		
+
 	}
 
 }
