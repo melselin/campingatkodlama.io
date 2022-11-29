19a89ecbf0798a010d8334ce251a617932e0678a
Melisa <melisaselinmusellim@gmail.com>
2022-11-29 23:38:49 +0300

    commit

--- kodlamaioIndex/src/kodlamaioIndex/dataAccess/HibernateCourseDao.java
+++ kodlamaioIndex/src/kodlamaioIndex/dataAccess/HibernateCourseDao.java
@@ -7,7 +7,7 @@ public class HibernateCourseDao implements CourseDao {
 
 	@Override
 	public void add(Course course) {
-		System.out.println(course.get_courseName()+" has been successfully added via hibernate.");
+		System.out.println(course.get_courseName()+  " with the id:"+ course.get_courseId() +" has been successfully added via hibernate.");
 		
 	}
 
--- kodlamaioIndex/src/kodlamaioIndex/dataAccess/JdbcCourseDao.java
+++ kodlamaioIndex/src/kodlamaioIndex/dataAccess/JdbcCourseDao.java
@@ -5,7 +5,7 @@ import kodlamaioIndex.entities.Course;
 public class JdbcCourseDao implements CourseDao{
 	@Override
 	public void add(Course course) {
-		System.out.println("Added via jdbc");
+		System.out.println(course.get_courseName()+  " with the id:"+ course.get_courseId() +" has been successfully added via jdbc");
 	}
 
 }
