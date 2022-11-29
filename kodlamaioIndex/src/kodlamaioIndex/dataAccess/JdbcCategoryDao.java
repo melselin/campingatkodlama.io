77f672764c36a180dd0389c2c40935a48cfece27
Melisa <melisaselinmusellim@gmail.com>
2022-11-29 23:35:25 +0300

    commit

--- kodlamaioIndex/src/kodlamaioIndex/dataAccess/JdbcCategoryDao.java
+++ kodlamaioIndex/src/kodlamaioIndex/dataAccess/JdbcCategoryDao.java
@@ -5,7 +5,7 @@ import kodlamaioIndex.entities.Category;
 public class JdbcCategoryDao implements CategoryDao{
 	@Override
 	public void add(Category category) {
-		System.out.println(category+ " has been successfully added via jdbc");
+		System.out.println(category.get_categoryType()+ " has been successfully added via jdbc");
 	}
 	
 
